87e3e4c3ea7893a6cb8eda7fb447903ebfe705a2
dagwaging <dagwaging@gmail.com>
2021-10-13 17:17:10 -0400

    Add native host

--- background.js
+++ background.js
@@ -1,38 +1,77 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  switch (message.type) {
-    case 'search':
-      chrome.downloads.search(
-        {},
-        (results) => {
-          sendResponse(
-            results.filter(
-              download =>
-                download.byExtensionId == chrome.runtime.id &&
-                download.exists &&
-                download.state == 'complete'
-            )
-          )
-        }
-      )
-      break
-    case 'download':
-      let filename = 'archive/' + message.name + '/' + message.filename // todo: make subdirectory name choosable
-      chrome.downloads.download(
-        {
-          url: message.url,
-          filename: filename,
-          conflictAction: 'uniquify'
-        },
-        (downloadId) => {
-          if (downloadId === undefined) {
-            sendResponse(chrome.runtime.lastError + ': ' + filename)
-          }
-          else {
-            sendResponse(null)
-          }
-        }
-      )
-      break
+let connections = 0
+let connection = null
+
+chrome.storage.onChanged.addListener((changes, areaName) => {
+  if (areaName == 'local' && changes.directory) {
+    console.log(changes.directory.newValue)
   }
-  return true
+})
+
+chrome.runtime.onConnect.addListener((port) => {
+  if (!connection) {
+    connection = chrome.runtime.connectNative('com.dagwaging.archive')
+  }
+
+  let listener = (message, _) => {
+    let error = chrome.runtime.lastError
+    if (error) {
+      port.postMessage({ error: error })
+    }
+    else {
+      message.type = 'get'
+      port.postMessage(message)
+
+      if (message.msg) {
+        port.postMessage({
+          type: 'suggestions',
+          msg: [...new Set(Object.values(message.msg))]
+        })
+      }
+    }
+  }
+
+  connection.onMessage.addListener(listener)
+
+  connections += 1
+  console.log('Connected; ' + connections + ' connected total')
+
+  port.onMessage.addListener((message, port) => {
+    console.log('Message received', message)
+
+    chrome.storage.local.get('directory', (items) => {
+      if (!items.directory) {
+        port.postMessage({ error: 'No directory set' })
+        return
+      }
+
+      switch (message.type) {
+        case 'get':
+          connection.postMessage({ "Get": { directory: items.directory } })
+          break
+        case 'set':
+          connection.postMessage({
+            "Set": {
+              directory: items.directory,
+              url: message.url,
+              hash: message.hash,
+              name: message.name,
+              filename: message.filename
+            }
+          })
+          break
+      }
+    })
+  })
+
+  port.onDisconnect.addListener((port) => {
+    connection.onMessage.removeListener(listener)
+
+    connections -= 1
+    console.log('Disconnected; ' + connections + ' connected total')
+
+    if (connections == 0) {
+      connection.disconnect()
+      connection = null
+    }
+  })
 })
